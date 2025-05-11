9ceb57c31c7d69ec2e47a93f4a50e67abc38c852
Jaygupta930 <jayguptajaygupta8@gmail.com>
2025-05-11 22:03:46 +0530

    feat: update About component button to include cursor pointer for better UX

--- myportfolio/src/components/About/About.jsx
+++ myportfolio/src/components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
           <a
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            className="inline-block text-white py-3 px-8 rounded-full cursor-pointer mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
               background: 'linear-gradient(90deg, #8245ec, #a855f7)',
               boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
