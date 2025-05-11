866b6156e7e2468b519a5d7f679b9df790dd8c54
Jaygupta930 <jayguptajaygupta8@gmail.com>
2025-05-11 22:03:14 +0530

    feat: update education and project logos, add new entries for skills and projects

--- myportfolio/src/constants.js
+++ myportfolio/src/constants.js
@@ -29,15 +29,23 @@ import figmaLogo from './assets/tech_logo/figma.png';
 // import agcLogo from './assets/company_logo/agc_logo.png';
 // import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
 
-// // Education Section Logo's
+//  Education Section Logo's
+import SsipmtLogo from './assets/edu_logo/ssipmt.jpg';
+import RamanUniversityLogo from './assets/edu_logo/cvraman.png';
+import NationalPublicSchoolLogo from './assets/edu_logo/national.jpg';
+import SaraswatiVidhyaMandirLogo from './assets/edu_logo/shishumandir.jpg';
 // import glaLogo from './assets/education_logo/gla_logo.png';
 // import bsaLogo from './assets/education_logo/bsa_logo.png';
 // import vpsLogo from './assets/education_logo/vps_logo.png';
 
 // Project Section Logo's
-// import githubdetLogo from './assets/work_logo/github_det.png';
-// import csprepLogo from './assets/work_logo/cs_prep.png';
-// import movierecLogo from './assets/work_logo/movie_rec.png';
+import JsonCodeEditorLogo from './assets/work_logo/json.png';
+import StoreRatingLogo from './assets/work_logo/storerating.png';
+import AdminPortalLogo from './assets/work_logo/adminportal.png';
+import HungryLogo from './assets/work_logo/hungryfood.png';
+import MusicLogo from './assets/work_logo/music.png';
+import FreshJuiceLogo from './assets/work_logo/freshjuice.png';
+
 // import taskremLogo from './assets/work_logo/task_rem.png';
 // import npmLogo from './assets/work_logo/npm.png';
 // import webverLogo from './assets/work_logo/web_dig.png';
@@ -154,131 +162,131 @@ export const SkillsInfo = [
   export const education = [
     {
       id: 0,
-      // img: glaLogo,
-      school: "GLA University, Mathura",
-      date: "Sept 2022 - July 2024",
+      img: SsipmtLogo,
+      school: "SSIPMT, Raipur",
+      date: "Oct 2022 - July 2026",
       grade: "7.81 CGPA",
-      desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-      degree: "Master of Computer Applications - MCA",
+      desc: "Bachelor of Technology in Computer Science and Engineering (B.Tech CSE) at SSIPMT, Raipur is a professional engineering program focused on the study of computer systems, programming, software development, data structures, algorithms, and emerging technologies like AI and machine learning. The program aims to build strong technical and analytical skills essential for a career in the software and IT industry.",
+      degree: "Bachelor of Technology in (CSE) - B.tech(CSE)",
     },
     {
       id: 1,
-      // img: bsaLogo,
-      school: "BSA College, Mathura",
+       img: RamanUniversityLogo,
+      school: "SSCE Computer Center, Wadrafnagar",
       date: "Sept 2018 - Aug 2021",
       grade: "73.2%",
-      desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-      degree: "Bachelor of Science - BSC (Computer Science)",
+      desc: "Diploma in Computer Applications (DCA) is a short-term program that provides foundational knowledge in computer and IT applications. It covers essential topics such as Microsoft Office tools, internet basics, computer fundamentals, database management, programming languages, and software development. This course is ideal for individuals looking to enhance their computer skills for professional or academic growth.",
+      degree: "Diploma in Computer Applications - DCA",
     },
-    {
-      id: 2,
-      // img: vpsLogo,
-      school: "Vatsalya Public School Govardhan, Mathura",
-      date: "Apr 2017 - March 2018",
-      grade: "78%",
-      desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
-      degree: "CBSE(XII) - PCM with Computer Science",
-    },
-    {
-      id: 3,
-      // img: vpsLogo,
-      school: "Vatsalya Public School Govardhan, Mathura",
-      date: "Apr 2015 - March 2016",
-      grade: "87.5%",
-      desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
-      degree: "CBSE(X), Science with Computer Application",
-    },
+    // {
+    //   id: 2,
+    //    img: NationalPublicSchoolLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2017 - March 2018",
+    //   grade: "78%",
+    //   desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    //   degree: "CBSE(XII) - PCM ",
+    // },
+    // {
+    //   id: 3,
+    //   img: SaraswatiVidhyaMandirLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2015 - March 2016",
+    //   grade: "87.5%",
+    //   desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    //   degree: "CBSE(X), Science with Computer Application",
+    // },
   ];
   
   export const projects = [
     {
       id: 0,
-      title: "GitHub Profile Detective",
+      title: "json_code_editor",
       description:
-        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
-      // image: githubdetLogo,
+        "A powerful, modern web-based JSON editor built using React. With intuitive features and real-time feedback, the JSON Code Editor allows users to seamlessly write, upload, validate, format, visualize, and share JSON data — all in one place.",
+      image: JsonCodeEditorLogo,
       tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
-      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
-      webapp: "https://githubprofiledetective.netlify.app/",
+      github: "https://github.com/JayGupta930/json_code_editor",
+      webapp: "https://json-code-editor.web.app/",
     },
     {
       id: 1,
-      title: "CS Prep",
+      title: "Store Rating",
       description:
-        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
-      // image: csprepLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/codingmastr/CSPrep",
-      webapp: "https://csprep.netlify.app/",
+        "A full-stack web application built with React.js and Supabase that allows users to register, log in, and rate stores. The platform supports three roles.......",
+       image: StoreRatingLogo,
+      tags: ["React JS", "Supabase", "Postgres", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/JayGupta930/store_rating",
+      webapp: "https://store-rating930.web.app/home",
     },
     {
       id: 2,
-      title: "Movie Recommendation App",
+      title: "Admin Portal",
       description:
-        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
-      // image: movierecLogo,
-      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/codingmastr/Movie-Recommendation-App",
-      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+        "A dedicated admin portal developed using React for managing a single medical device. The portal provides a clean and user-friendly interface to monitor device data, manage settings, and ensure smooth operation, tailored specifically for streamlined control and maintenance.",
+       image: AdminPortalLogo,
+      tags: ["React JS", "API", "Supabase", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/JayGupta930/uropine_admin_portal",
+      webapp: "https://console.uropine.ruralsurgery.in/dashboard",
     },
     {
       id: 3,
-      title: "Email Validator NPM Package",
+      title: "UI of Music Web App",
       description:
-        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
-      // image: npmLogo,
-      tags: ["React JS", "Node.js", "NPM", "Validation"],
-      github: "https://github.com/codingmastr/cmtk-email-validator",
-      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+        "Stream top albums, discover artists, enjoy podcasts, and vibe to playlists made just for you.",
+       image: MusicLogo,
+      tags: ["Figma"],
+      github: "https://www.figma.com/@Jay930",
+      webapp: "hhttps://www.figma.com/@Jay930",
     },
     {
       id: 4,
-      title: "Task Reminder Chrome Extension Tool",
+      title: "UI of Food Delivery App",
       description:
-        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
-      // image: taskremLogo,
-      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
-      github: "https://github.com/codingmastr/Task-Reminder-Tool",
-      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+        "Search nearby restaurants, pick your favorite dishes, and enjoy fast delivery right to your door. Deliciousness made easy!",
+       image: HungryLogo,
+      tags: ["Figma"],
+      github: "https://www.figma.com/@Jay930",
+      webapp: "https://www.figma.com/@Jay930",
     },
     {
       id: 5,
-      title: "Webverse Digital",
+      title: "UI of Fresh Juice Web App",
       description:
-        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
-      // image: webverLogo,
-      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
-      github: "https://github.com/codingmastr/Webverse-Digital",
-      webapp: "https://webversedigital.com/",
+        "From orchard to glass — discover refreshing, natural juices packed with flavor and goodness. Taste the zest of life with every sip!",
+       image: FreshJuiceLogo,
+      tags: [ "Figma"],
+      github: "https://www.figma.com/@Jay930",
+      webapp: "https://www.figma.com/@Jay930",
     },
-    {
-      id: 6,
-      title: "Coding Master",
-      description:
-        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-      // image: cmLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-      github: "https://codingmasterweb.in/",
-      webapp: "https://codingmasterweb.in/",
-    },
-    {
-      id: 7,
-      title: "Image Search App",
-      description:
-        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-      // image: imagesearchLogo,
-      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-      github: "https://github.com/codingmastr/Image-Search-App",
-      webapp: "https://imagsearch.netlify.app/",
-    },
-    {
-      id: 8,
-      title: "Image Background Remover",
-      description:
-        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-      // image: removebgLogo,
-      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-      github: "https://github.com/codingmastr/Image-Background-Remover",
-      webapp: "https://removeyourbg.netlify.app/",
-    },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   // image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   // image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   // image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
   ];  
